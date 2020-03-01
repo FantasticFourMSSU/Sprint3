641b9729384d22180bf4c797b7fafe379a6f699f
Matt Sommer <SommerM001@mymail.mssu.edu>
2020-03-01 16:00:46 -0600

    Matt Sommer

---
+++ LECCISAddProperty/AddProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LECCISAddProperty
+{
+  public partial class addPropertyForm : Form
+  {
+    public addPropertyForm()
+    {
+      InitializeComponent();
+    }
+  }
+}
