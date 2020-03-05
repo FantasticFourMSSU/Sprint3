cdc58c622f7875202fa1c330df8c442259e89552
Laura Remsky <RemskyL001@mymail.mssu.edu>
2020-03-05 12:56:25 -0600

    AddProperty

--- LECCISAddProperty/AddProperty.Designer.cs
+++ LECCISAddProperty/AddProperty.Designer.cs
@@ -36,11 +36,16 @@
       this.Zip = new System.Windows.Forms.TextBox();
       this.Property = new System.Windows.Forms.Label();
       this.addRecord = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
       this.SuspendLayout();
       // 
       // Address
       // 
-      this.Address.ForeColor = System.Drawing.Color.Gray;
+      
       resources.ApplyResources(this.Address, "Address");
       this.Address.Name = "Address";
       this.Address.UseWaitCursor = true;
@@ -49,28 +54,28 @@
       // 
       // City
       // 
-      this.City.ForeColor = System.Drawing.Color.Gray;
+      
       resources.ApplyResources(this.City, "City");
       this.City.Name = "City";
       this.City.UseWaitCursor = true;
       // 
       // State
       // 
-      this.State.ForeColor = System.Drawing.Color.Gray;
+     
       resources.ApplyResources(this.State, "State");
       this.State.Name = "State";
       this.State.UseWaitCursor = true;
       // 
       // comboBox1
       // 
-      this.comboBox1.ForeColor = System.Drawing.Color.Gray;
+     
       this.comboBox1.FormattingEnabled = true;
       resources.ApplyResources(this.comboBox1, "comboBox1");
       this.comboBox1.Name = "comboBox1";
       // 
       // Zip
       // 
-      this.Zip.ForeColor = System.Drawing.Color.Gray;
+     
       resources.ApplyResources(this.Zip, "Zip");
       this.Zip.Name = "Zip";
       this.Zip.UseWaitCursor = true;
@@ -86,11 +91,41 @@
       this.addRecord.Name = "addRecord";
       this.addRecord.UseVisualStyleBackColor = true;
       // 
+      // label1
+      // 
+      resources.ApplyResources(this.label1, "label1");
+      this.label1.Name = "label1";
+      // 
+      // label2
+      // 
+      resources.ApplyResources(this.label2, "label2");
+      this.label2.Name = "label2";
+      // 
+      // label3
+      // 
+      resources.ApplyResources(this.label3, "label3");
+      this.label3.Name = "label3";
+      // 
+      // label4
+      // 
+      resources.ApplyResources(this.label4, "label4");
+      this.label4.Name = "label4";
+      // 
+      // label5
+      // 
+      resources.ApplyResources(this.label5, "label5");
+      this.label5.Name = "label5";
+      // 
       // addProperty
       // 
       resources.ApplyResources(this, "$this");
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
       this.Controls.Add(this.addRecord);
       this.Controls.Add(this.Property);
       this.Controls.Add(this.Zip);
@@ -113,6 +148,11 @@
     private System.Windows.Forms.TextBox Zip;
     private System.Windows.Forms.Label Property;
     private System.Windows.Forms.Button addRecord;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label label5;
   }
 }
 
