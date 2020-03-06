acea47b39281b8c2e4e5140db92b898b82173515
Matt Sommer <SommerM001@mymail.mssu.edu>
2020-03-05 21:48:43 -0600

    Still need to add SQL Connection String

--- LECCISSearch/Search.Designer.cs
+++ LECCISSearch/Search.Designer.cs
@@ -28,121 +28,122 @@
     /// </summary>
     private void InitializeComponent()
     {
-      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(searchForm));
-      this.searchButton = new System.Windows.Forms.Button();
-      this.searchOwnerFNTextBox = new System.Windows.Forms.TextBox();
-      this.searchOwnerLNTextBox = new System.Windows.Forms.TextBox();
-      this.searchPropertyStreetTextBox = new System.Windows.Forms.TextBox();
-      this.searchPropertyStreetLabel = new System.Windows.Forms.Label();
-      this.searchOwnerFNLabel = new System.Windows.Forms.Label();
-      this.searchOwnerLNLabel = new System.Windows.Forms.Label();
-      this.searchOwnerLabel = new System.Windows.Forms.Label();
-      this.searchPropertyLabel = new System.Windows.Forms.Label();
-      this.SuspendLayout();
-      // 
-      // searchButton
-      // 
-      this.searchButton.Location = new System.Drawing.Point(796, 844);
-      this.searchButton.Name = "searchButton";
-      this.searchButton.Size = new System.Drawing.Size(206, 66);
-      this.searchButton.TabIndex = 0;
-      this.searchButton.Text = "Search";
-      this.searchButton.UseVisualStyleBackColor = true;
-      // 
-      // searchOwnerFNTextBox
-      // 
-      this.searchOwnerFNTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.searchOwnerFNTextBox.Location = new System.Drawing.Point(324, 223);
-      this.searchOwnerFNTextBox.Name = "searchOwnerFNTextBox";
-      this.searchOwnerFNTextBox.Size = new System.Drawing.Size(300, 38);
-      this.searchOwnerFNTextBox.TabIndex = 1;
-      // 
-      // searchOwnerLNTextBox
-      // 
-      this.searchOwnerLNTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.searchOwnerLNTextBox.Location = new System.Drawing.Point(324, 323);
-      this.searchOwnerLNTextBox.Name = "searchOwnerLNTextBox";
-      this.searchOwnerLNTextBox.Size = new System.Drawing.Size(300, 38);
-      this.searchOwnerLNTextBox.TabIndex = 2;
-      // 
-      // searchPropertyStreetTextBox
-      // 
-      this.searchPropertyStreetTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.searchPropertyStreetTextBox.Location = new System.Drawing.Point(1403, 223);
-      this.searchPropertyStreetTextBox.Name = "searchPropertyStreetTextBox";
-      this.searchPropertyStreetTextBox.Size = new System.Drawing.Size(438, 38);
-      this.searchPropertyStreetTextBox.TabIndex = 3;
-      // 
-      // searchPropertyStreetLabel
-      // 
-      this.searchPropertyStreetLabel.AutoSize = true;
-      this.searchPropertyStreetLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.searchPropertyStreetLabel.Location = new System.Drawing.Point(1146, 223);
-      this.searchPropertyStreetLabel.Name = "searchPropertyStreetLabel";
-      this.searchPropertyStreetLabel.Size = new System.Drawing.Size(228, 37);
-      this.searchPropertyStreetLabel.TabIndex = 4;
-      this.searchPropertyStreetLabel.Text = "Street Address";
-      // 
-      // searchOwnerFNLabel
-      // 
-      this.searchOwnerFNLabel.AutoSize = true;
-      this.searchOwnerFNLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.searchOwnerFNLabel.Location = new System.Drawing.Point(67, 223);
-      this.searchOwnerFNLabel.Name = "searchOwnerFNLabel";
-      this.searchOwnerFNLabel.Size = new System.Drawing.Size(175, 37);
-      this.searchOwnerFNLabel.TabIndex = 5;
-      this.searchOwnerFNLabel.Text = "First Name";
-      // 
-      // searchOwnerLNLabel
-      // 
-      this.searchOwnerLNLabel.AutoSize = true;
-      this.searchOwnerLNLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.searchOwnerLNLabel.Location = new System.Drawing.Point(67, 323);
-      this.searchOwnerLNLabel.Name = "searchOwnerLNLabel";
-      this.searchOwnerLNLabel.Size = new System.Drawing.Size(173, 37);
-      this.searchOwnerLNLabel.TabIndex = 6;
-      this.searchOwnerLNLabel.Text = "Last Name";
-      // 
-      // searchOwnerLabel
-      // 
-      this.searchOwnerLabel.AutoSize = true;
-      this.searchOwnerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.searchOwnerLabel.Location = new System.Drawing.Point(244, 114);
-      this.searchOwnerLabel.Name = "searchOwnerLabel";
-      this.searchOwnerLabel.Size = new System.Drawing.Size(266, 37);
-      this.searchOwnerLabel.TabIndex = 7;
-      this.searchOwnerLabel.Text = "Search By Owner";
-      // 
-      // searchPropertyLabel
-      // 
-      this.searchPropertyLabel.AutoSize = true;
-      this.searchPropertyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.searchPropertyLabel.Location = new System.Drawing.Point(1396, 114);
-      this.searchPropertyLabel.Name = "searchPropertyLabel";
-      this.searchPropertyLabel.Size = new System.Drawing.Size(292, 37);
-      this.searchPropertyLabel.TabIndex = 8;
-      this.searchPropertyLabel.Text = "Search By Property";
-      // 
-      // searchForm
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-      this.ClientSize = new System.Drawing.Size(2140, 1314);
-      this.Controls.Add(this.searchPropertyLabel);
-      this.Controls.Add(this.searchOwnerLabel);
-      this.Controls.Add(this.searchOwnerLNLabel);
-      this.Controls.Add(this.searchOwnerFNLabel);
-      this.Controls.Add(this.searchPropertyStreetLabel);
-      this.Controls.Add(this.searchPropertyStreetTextBox);
-      this.Controls.Add(this.searchOwnerLNTextBox);
-      this.Controls.Add(this.searchOwnerFNTextBox);
-      this.Controls.Add(this.searchButton);
-      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-      this.Name = "searchForm";
-      this.Text = "Law Enforcement Community Check Information System (LECCIS)";
-      this.ResumeLayout(false);
-      this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(searchForm));
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchOwnerFNTextBox = new System.Windows.Forms.TextBox();
+            this.searchOwnerLNTextBox = new System.Windows.Forms.TextBox();
+            this.searchPropertyStreetTextBox = new System.Windows.Forms.TextBox();
+            this.searchPropertyStreetLabel = new System.Windows.Forms.Label();
+            this.searchOwnerFNLabel = new System.Windows.Forms.Label();
+            this.searchOwnerLNLabel = new System.Windows.Forms.Label();
+            this.searchOwnerLabel = new System.Windows.Forms.Label();
+            this.searchPropertyLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(912, 844);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(206, 66);
+            this.searchButton.TabIndex = 0;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // searchOwnerFNTextBox
+            // 
+            this.searchOwnerFNTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchOwnerFNTextBox.Location = new System.Drawing.Point(440, 223);
+            this.searchOwnerFNTextBox.Name = "searchOwnerFNTextBox";
+            this.searchOwnerFNTextBox.Size = new System.Drawing.Size(300, 38);
+            this.searchOwnerFNTextBox.TabIndex = 1;
+            // 
+            // searchOwnerLNTextBox
+            // 
+            this.searchOwnerLNTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchOwnerLNTextBox.Location = new System.Drawing.Point(440, 323);
+            this.searchOwnerLNTextBox.Name = "searchOwnerLNTextBox";
+            this.searchOwnerLNTextBox.Size = new System.Drawing.Size(300, 38);
+            this.searchOwnerLNTextBox.TabIndex = 2;
+            // 
+            // searchPropertyStreetTextBox
+            // 
+            this.searchPropertyStreetTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchPropertyStreetTextBox.Location = new System.Drawing.Point(1519, 223);
+            this.searchPropertyStreetTextBox.Name = "searchPropertyStreetTextBox";
+            this.searchPropertyStreetTextBox.Size = new System.Drawing.Size(438, 38);
+            this.searchPropertyStreetTextBox.TabIndex = 3;
+            // 
+            // searchPropertyStreetLabel
+            // 
+            this.searchPropertyStreetLabel.AutoSize = true;
+            this.searchPropertyStreetLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchPropertyStreetLabel.Location = new System.Drawing.Point(1262, 223);
+            this.searchPropertyStreetLabel.Name = "searchPropertyStreetLabel";
+            this.searchPropertyStreetLabel.Size = new System.Drawing.Size(228, 37);
+            this.searchPropertyStreetLabel.TabIndex = 4;
+            this.searchPropertyStreetLabel.Text = "Street Address";
+            // 
+            // searchOwnerFNLabel
+            // 
+            this.searchOwnerFNLabel.AutoSize = true;
+            this.searchOwnerFNLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchOwnerFNLabel.Location = new System.Drawing.Point(183, 223);
+            this.searchOwnerFNLabel.Name = "searchOwnerFNLabel";
+            this.searchOwnerFNLabel.Size = new System.Drawing.Size(175, 37);
+            this.searchOwnerFNLabel.TabIndex = 5;
+            this.searchOwnerFNLabel.Text = "First Name";
+            // 
+            // searchOwnerLNLabel
+            // 
+            this.searchOwnerLNLabel.AutoSize = true;
+            this.searchOwnerLNLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchOwnerLNLabel.Location = new System.Drawing.Point(183, 323);
+            this.searchOwnerLNLabel.Name = "searchOwnerLNLabel";
+            this.searchOwnerLNLabel.Size = new System.Drawing.Size(173, 37);
+            this.searchOwnerLNLabel.TabIndex = 6;
+            this.searchOwnerLNLabel.Text = "Last Name";
+            // 
+            // searchOwnerLabel
+            // 
+            this.searchOwnerLabel.AutoSize = true;
+            this.searchOwnerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchOwnerLabel.Location = new System.Drawing.Point(360, 114);
+            this.searchOwnerLabel.Name = "searchOwnerLabel";
+            this.searchOwnerLabel.Size = new System.Drawing.Size(266, 37);
+            this.searchOwnerLabel.TabIndex = 7;
+            this.searchOwnerLabel.Text = "Search By Owner";
+            // 
+            // searchPropertyLabel
+            // 
+            this.searchPropertyLabel.AutoSize = true;
+            this.searchPropertyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchPropertyLabel.Location = new System.Drawing.Point(1512, 114);
+            this.searchPropertyLabel.Name = "searchPropertyLabel";
+            this.searchPropertyLabel.Size = new System.Drawing.Size(292, 37);
+            this.searchPropertyLabel.TabIndex = 8;
+            this.searchPropertyLabel.Text = "Search By Property";
+            // 
+            // searchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(2140, 1314);
+            this.Controls.Add(this.searchPropertyLabel);
+            this.Controls.Add(this.searchOwnerLabel);
+            this.Controls.Add(this.searchOwnerLNLabel);
+            this.Controls.Add(this.searchOwnerFNLabel);
+            this.Controls.Add(this.searchPropertyStreetLabel);
+            this.Controls.Add(this.searchPropertyStreetTextBox);
+            this.Controls.Add(this.searchOwnerLNTextBox);
+            this.Controls.Add(this.searchOwnerFNTextBox);
+            this.Controls.Add(this.searchButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "searchForm";
+            this.Text = "Law Enforcement Community Check Information System (LECCIS)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
--- LECCISSearch/Search.cs
+++ LECCISSearch/Search.cs
@@ -7,14 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace LECCISSearch
 {
-  public partial class searchForm : Form
-  {
-    public searchForm()
+
+    public partial class searchForm : Form
     {
-      InitializeComponent();
+        public searchForm()
+        {
+            InitializeComponent();
+
+            string connString = "Server= 209.106.201.103; Database=group6; uid=dbstudent14;pwd=spicymonster10";
+            MySqlConnection conn = new MySqlConnection(connString);
+            
+        }
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            if (searchOwnerFNTextBox.TextLength == 0)
+            {
+                if (searchOwnerLNTextBox.TextLength == 0)
+                {
+                    if (searchPropertyStreetTextBox.TextLength == 0)
+                    {
+                        MessageBox.Show("Must search at least one variable.", "Invalid Input", MessageBoxButtons.OK);
+                        searchOwnerFNTextBox.Clear();
+                        searchOwnerLNTextBox.Clear();
+                        searchPropertyStreetTextBox.Clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Searching by Street Address.", "Search Message", MessageBoxButtons.OK);
+                        searchOwnerFNTextBox.Clear();
+                        searchOwnerLNTextBox.Clear();
+                        searchPropertyStreetTextBox.Clear();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Searching by lastname.", "Search Message", MessageBoxButtons.OK);
+                    searchOwnerFNTextBox.Clear();
+                    searchOwnerLNTextBox.Clear();
+                    searchPropertyStreetTextBox.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Searching by firstname.", "Search Message", MessageBoxButtons.OK);
+                searchOwnerFNTextBox.Clear();
+                searchOwnerLNTextBox.Clear();
+                searchPropertyStreetTextBox.Clear();
+            }
+        }
+
     }
-  }
+
 }
